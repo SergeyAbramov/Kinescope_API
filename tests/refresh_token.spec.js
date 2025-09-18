573208730197c5535ad20d5edf80a7f2811bdf62
Sergey Abramov <lipkifarm@gmail.com>
2025-09-18 22:18:18 +0300

    Some minor fixes

--- tests/refresh_token.spec.js
+++ tests/refresh_token.spec.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 test.describe('@refresh_token.spec.js @DEV Test that we can refresh token via API', async () => {
     test.skip('1. Refresh token', async ({ request }) => {
 
-        const response = await request.get('https://api-dev.bigvu.tv/v0.5/oauth/token?grant_type=refresh_token&refresh_token=MFRRdUUwV05vMy9EQTBZS2RaQm45SXNXVGpjdmRPRU5XdHBsdExta1JoVmZtYzd5b1c1UC9QTXlsV1VYTFFleVFwRStlRGxpZDMwPQ%3D%3D', {
+        const response = await request.get('https://api-dev.bigvu.tv/v0.5/oauth/token?grant_type=refresh_token&refresh_token={process.env.AUTHORIZATION_TOKEN}', {
 
             headers: {
                 ['Content-type']: 'application/json',
